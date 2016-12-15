93082236caa7dddd5de8303376186fb804583290
detree <ssong18@illinois.edu>
2016-12-15 11:40:26 -0600

    ip change

--- src/main/java/testmain/BotInfoSelector.java
+++ src/main/java/testmain/BotInfoSelector.java
@@ -44,7 +44,7 @@ public class BotInfoSelector {
             }
             else if(type == Common.ARDRONE2) {
                 this.type = new ModelARDrone2(name, 0,0,0);
-                this.ip = "192.168.0.104";
+                this.ip = "192.168.0.16";
             }
 
         }
@@ -67,7 +67,7 @@ public class BotInfoSelector {
             }
             else if(type == Common.ARDRONE2) {
                 this.type = new ModelARDrone2(name, 0,0,0);
-                this.ip = "192.168.0.103";
+                this.ip = "192.168.0.15";
             }
         }
 
