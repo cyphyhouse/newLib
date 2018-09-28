20e78d973a2c3d06be0d9d66d6868710a4f728f6
Peter Du <peterdu2@illinois.edu>
2018-09-28 14:49:36 -0500

    deleted more unused files

--- src/main/java/edu/illinois/mitra/cyphyhouse/interfaces/ExplicitlyDrawable.java
+++
@@ -1,8 +0,0 @@
-package edu.illinois.mitra.cyphyhouse.interfaces;
-
-import java.util.Collection;
-
-public interface ExplicitlyDrawable
-{
-	public void drawNow(Collection <LogicThread> lts);
-}
--- src/main/java/testmain/TestMain.java
+++ src/main/java/testmain/TestMain.java
@@ -13,11 +13,11 @@ import java.util.HashMap;
 public class TestMain {
         // Row 0 = names
         // Row 1 = MACs
-        // Row 2 = IPs
+        // Row 2 = IP
         private static String[][] participants;
         private static int numRobots;
         private static BotInfoSelector[] botInfo;
-        private static int selectedRobot = 1;
+        private static int selectedRobot = 0;
         volatile private static GlobalVarHolder gvh;
         private static LogicThread appToRun = null;
 
