dd0579ca5110a0726572338bbf11baaf16666347
ritwika314 <mim.ghosh@gmail.com>
2018-10-04 11:18:03 -0500

    update

--- src/main/java/testSim/follow/FollowApp.java
+++ src/main/java/testSim/follow/FollowApp.java
@@ -6,66 +6,55 @@ package testSim.follow;
  * This app was created to test the drones. The bots will each go to an assigned waypoint.
  * Once both bots have arrived at their respective waypoints, they will then go to the next waypoints.
  */
-/**
- * Created by VerivitalLab on 2/26/2016.
- * This app was created to test the drones. The bots will each go to an assigned waypoint.
- * Once both bots have arrived at their respective waypoints, they will then go to the next waypoints.
- */
-import java.net.*;
-import java.io.*;
-import java.util.*;
-import java.lang.*;
-import java.nio.file.*;
-import java.util.stream.Stream;
 
-import edu.illinois.mitra.cyphyhouse.functions.DSMMultipleAttr;
 import edu.illinois.mitra.cyphyhouse.comms.RobotMessage;
+import edu.illinois.mitra.cyphyhouse.functions.DSMMultipleAttr;
+import edu.illinois.mitra.cyphyhouse.functions.GroupSetMutex;
 import edu.illinois.mitra.cyphyhouse.gvh.GlobalVarHolder;
+import edu.illinois.mitra.cyphyhouse.interfaces.DSM;
 import edu.illinois.mitra.cyphyhouse.interfaces.LogicThread;
+import edu.illinois.mitra.cyphyhouse.interfaces.MutualExclusion;
 import edu.illinois.mitra.cyphyhouse.motion.MotionParameters;
-import edu.illinois.mitra.cyphyhouse.motion.RRTNode;
 import edu.illinois.mitra.cyphyhouse.motion.MotionParameters.COLAVOID_MODE_TYPE;
 import edu.illinois.mitra.cyphyhouse.objects.ItemPosition;
-import edu.illinois.mitra.cyphyhouse.objects.ObstacleList;
-import edu.illinois.mitra.cyphyhouse.objects.PositionList;
-import edu.illinois.mitra.cyphyhouse.interfaces.DSM;
-import edu.illinois.mitra.cyphyhouse.functions.GroupSetMutex;
-import edu.illinois.mitra.cyphyhouse.interfaces.MutualExclusion;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Stream;
 
 
 public class FollowApp extends LogicThread {
     private static final String TAG = "Follow App";
-    private static final int DEST_MSG = 23;
-    private static final int ERASE_MSG = 24;
-    int lineno = 0;
-    int eraseline = -1;
-    private int numBots;
-    boolean init = true;
-    private int numWaypoints;
-    private boolean arrived = false;
-    private boolean goForever = false;
+
     private int robotIndex;
+
+    //Dsm initializations
     private DSM dsm;
     private boolean wait0 = false;
-    private MutualExclusion mutex0; 
-    boolean dgt = false;
-    boolean takeoff = false;
-    private HashSet<RobotMessage> receivedMsgs = new HashSet<RobotMessage>();
-    private HashSet<RobotMessage> erasedMsgs = new HashSet<RobotMessage>();
-  
+    private MutualExclusion mutex0;
 
+    //reading from ui
+    int lineno = 0;
+    private static final int DEST_MSG = 23;
+    private HashSet<RobotMessage> receivedMsgs = new HashSet<RobotMessage>();
     final Map<String, ItemPosition> destinations = new HashMap<String, ItemPosition>();
-    final Map<String, Integer> erasemap = new HashMap<String, Integer>();
-    // execute a function that takes a string and returns a string
+
+
+    //motion module declaration
     ItemPosition currentDestination;
-    ItemPosition currentPosition;
+
+    boolean dgt = false;
+    private boolean arrived = false;
+
     private enum Stage {
         PICK, GO, DONE, WAIT
-    }; 
-    private int index;
+    }
+
     public int testindex = 0;
     private Stage stage = Stage.PICK;
-    boolean connected = false;
+
     public FollowApp(GlobalVarHolder gvh) {
         super(gvh);
         MotionParameters.Builder settings = new MotionParameters.Builder();
@@ -74,105 +63,92 @@ public class FollowApp extends LogicThread {
         MotionParameters param = settings.build();
         gvh.plat.moat.setParameters(param);
         gvh.comms.addMsgListener(this, DEST_MSG);
-        gvh.comms.addMsgListener(this, ERASE_MSG);
 
         // bot names must be bot0, bot1, ... botn for this to work
         String intValue = name.replaceAll("[^0-9]", "");
         robotIndex = Integer.parseInt(intValue);
-        numBots = gvh.id.getParticipants().size();
-        dsm = new DSMMultipleAttr(gvh); 
-        mutex0 = new GroupSetMutex(gvh,0);
+        dsm = new DSMMultipleAttr(gvh);
+        mutex0 = new GroupSetMutex(gvh, 0);
 
     }
 
     @Override
     public List<Object> callStarL() {
-        dsm.createMW("testindex",0);
-       
-        while(true) {
-            switch(stage) {
+        dsm.createMW("testindex", 0);
+
+        while (true) {
+            switch (stage) {
                 case PICK:
                     arrived = false;
-		    String line;
-                        try (Stream<String> lines = Files.lines(Paths.get("tasks.txt"))) {
-                           line = lines.skip(lineno).findFirst().get();      
-                           RobotMessage inform = new RobotMessage("ALL", name, DEST_MSG, line);
-                           gvh.comms.addOutgoingMessage(inform);
-                           lineno  = lineno +1;
-     
-                        }
-                        catch (IOException e) {}
-                        catch (NoSuchElementException e) {stage = Stage.DONE;lineno = lineno - 1;}
-                        catch (IllegalArgumentException e) {stage = Stage.DONE;lineno = 0;}
-                    if(destinations.isEmpty()||robotIndex == 0) {
+                    if (robotIndex == 0) {
+                        updatedests("tasks.txt", DEST_MSG, name, lineno);
+                        lineno = lineno + 1;
+                    }
+                    if (destinations.isEmpty() || robotIndex == 0) {
                         stage = Stage.WAIT;
                     } else {
                         if (testindex >= destinations.size()) {
-                           stage = Stage.WAIT;
-                           break;
+                            stage = Stage.WAIT;
+                            break;
                         }
                         try {
-                        if(!wait0){	
-						mutex0.requestEntry(0);
-						wait0 = true;
-						System.out.println("here"+robotIndex);
-                                                break;
-					}
-					if(mutex0.clearToEnter(0)){
+                            if (!wait0) {
+                                mutex0.requestEntry(0);
+                                wait0 = true;
+                                break;
+                            }
+                            if (mutex0.clearToEnter(0)) {
+                                testindex = Integer.parseInt(dsm.get("testindex", "*"));
+                                currentDestination = getDestination(destinations, testindex);
+                                testindex = testindex + 1;
+                                dsm.put("testindex", "*", testindex);
+                                mutex0.exit(0);
 
-						testindex = testindex +1;
-                                                System.out.println("incrementing testindex "+robotIndex);
-						dsm.put("testindex", "*", testindex + 1);
-						mutex0.exit(0);
-                                                  
-					}
-                                       else {
-                                            break;
-                                       }
-                        currentDestination = getDestination(destinations, testindex);
+                            } else {
+                                break;
+                            }
+                        } catch (NullPointerException e) {
+                            stage = Stage.DONE;
+                            //lineno = lineno - 1;
+                            break;
                         }
-                        catch (NullPointerException e) {stage = Stage.DONE;lineno = lineno - 1; break;}
-                        if (currentDestination == null){
-                           stage = Stage.DONE;
-                           break;
+                        if (currentDestination == null) {
+                            stage = Stage.DONE;
+                            break;
                         }
 
                         gvh.plat.moat.goTo(currentDestination);
                         dgt = true;
-			if (currentDestination.getZ() == 0){
-			      stage = Stage.DONE;
-			} 
-
-                        else {
+                        if (currentDestination.getZ() == 0) {
+                            stage = Stage.DONE;
+                        } else {
                             stage = Stage.GO;
-                        }}
+                        }
+                    }
                     break;
                 case GO:
-                    if(!gvh.plat.moat.inMotion) {
-                       if (!arrived && currentDestination != null){
-                          stage = Stage.WAIT;}
-                       else {
-                          if (dgt == true) {
-                              eraseline++;
-                              RobotMessage erase = new RobotMessage("ALL",name, ERASE_MSG, Integer.toString(eraseline));
-                              gvh.comms.addOutgoingMessage(erase);
-                              dgt = false;
-                               wait0 = false;
-                       }
-                          stage = Stage.PICK;
-                          break;
-                       }
-                       arrived = true;
-                         
+                    if (!gvh.plat.moat.inMotion) {
+                        if (!arrived && currentDestination != null) {
+                            stage = Stage.WAIT;
+                        } else {
+                            if (dgt == true) {
+                                dgt = false;
+                                wait0 = false;
+                            }
+                            stage = Stage.PICK;
+                            break;
+                        }
+                        arrived = true;
+
 
                     }
                     break;
                 case WAIT:
-                    if (arrived && robotIndex != 0) { 
-                       stage = Stage.PICK;
+                    if (arrived && robotIndex != 0) {
+                        stage = Stage.PICK;
                     }
                     if (robotIndex == 0)
-                       stage = Stage.PICK;
+                        stage = Stage.PICK;
                     stage = Stage.GO;
                     break;
                 case DONE:
@@ -185,52 +161,39 @@ public class FollowApp extends LogicThread {
     @Override
     protected void receive(RobotMessage m) {
         boolean alreadyReceived = false;
-        for(RobotMessage msg : receivedMsgs) {
-            if(msg.getFrom().equals(m.getFrom()) && msg.getContents().equals(m.getContents())) {
+        for (RobotMessage msg : receivedMsgs) {
+            if (msg.getFrom().equals(m.getFrom()) && msg.getContents().equals(m.getContents())) {
                 alreadyReceived = true;
                 break;
             }
         }
-        for(RobotMessage msg : erasedMsgs) {
-            if(msg.getFrom().equals(m.getFrom()) && msg.getContents().equals(m.getContents())) {
-                alreadyReceived = true;
-                break;
-            }
-        }
-       int i = receivedMsgs.size(); 
-       int j = erasedMsgs.size(); 
-       if (m.getMID() == DEST_MSG && !m.getFrom().equals(name) && !alreadyReceived) {
+        int i = receivedMsgs.size();
+        if (m.getMID() == DEST_MSG && !m.getFrom().equals(name) && !alreadyReceived) {
             receivedMsgs.add(m);
             gvh.log.d(TAG, "received destination message from " + m.getFrom());
 
             String dest = m.getContents().toString();
-            dest = dest.replace(" ",",").replace("`","");
+            dest = dest.replace(" ", ",").replace("`", "");
             String[] parts = dest.split(",");
-            int x = (int) (Float.parseFloat(parts[0])*1000);
-            int y = (int) (Float.parseFloat(parts[1])*1000);
-            int z = (int) (Float.parseFloat(parts[2])*1000);
-            String name = Integer.toString(i) +"-A";
-            ItemPosition p = new ItemPosition(name,x,y,z);
-            destinations.put(p.getName(),p);
-              
-        }
-       if (m.getMID() == ERASE_MSG && !m.getFrom().equals(name) && !alreadyReceived) {
-            erasedMsgs.add(m);
-            gvh.log.d(TAG, "received erase message from " + m.getFrom());
-            String linenum = m.getContents().toString().replace("`","");
-            int x = Integer.parseInt(linenum);
-            try(FileWriter fw = new FileWriter("erase.txt", true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter out = new PrintWriter(bw))
-            {
-               out.println(x);
-              } catch (IOException e) {
-               //exception handling left as an exercise for the reader
-               }
+            int x = (int) (Float.parseFloat(parts[0]) * 1000);
+            int y = (int) (Float.parseFloat(parts[1]) * 1000);
+            int z = (int) (Float.parseFloat(parts[2]) * 1000);
+            String name = Integer.toString(i) + "-A";
+            ItemPosition p = new ItemPosition(name, x, y, z);
+            destinations.put(p.getName(), p);
 
         }
-       
-       
+    }
+
+    private void updatedests(String filename, int msgtype, String robotname, int lineno) {
+        try (Stream<String> lines = Files.lines(Paths.get(filename))) {
+            String line = lines.skip(lineno).findFirst().get();
+            RobotMessage inform = new RobotMessage("ALL", robotname, msgtype, line);
+            gvh.comms.addOutgoingMessage(inform);
+        } catch (IOException e) {
+        } catch (NoSuchElementException e) {
+        } catch (IllegalArgumentException e) {
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -241,4 +204,4 @@ public class FollowApp extends LogicThread {
         //String key = Integer.toString(index);
         return map.get(key);
     }
-} 
+}
