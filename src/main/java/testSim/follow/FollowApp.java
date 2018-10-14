6628ca72993904b94a789fb1105aea3bc5dd8dd1
ritwika314 <mim.ghosh@gmail.com>
2018-10-14 13:11:20 -0500

    update

--- src/main/java/testSim/follow/FollowApp.java
+++ src/main/java/testSim/follow/FollowApp.java
@@ -48,7 +48,7 @@ public class FollowApp extends LogicThread {
 
     //list of destinations
     final Map<String, ItemPosition> destinations = new HashMap<String, ItemPosition>();
-    final Map<String, Task> taskLocations = new HashMap<String,Task>();
+    final Map<String, Task> taskLocations = new HashMap<String, Task>();
 
 
     public GlobalVarHolder gvh;
@@ -125,9 +125,8 @@ public class FollowApp extends LogicThread {
 
                             if (updatePath) {
 
-                            }
-                            else{
-                                RobotMessage pathmsg = new RobotMessage("ALL", name, PATH_MSG, gvh.gps.getMyPosition().toString()+"###mypos");
+                            } else {
+                                RobotMessage pathmsg = new RobotMessage("ALL", name, PATH_MSG, gvh.gps.getMyPosition().toString() + "###mypos");
                                 gvh.comms.addOutgoingMessage(pathmsg);
                                 updatePath = false;
 
@@ -152,14 +151,14 @@ public class FollowApp extends LogicThread {
                                 //pathnode = new RRTNode(mypos.x,mypos.y);
                                 //Obstacles o = new Obstacles(1,1,1);
                                 //Vector<Obstacles> v = new Vector<>();
-                                SimplePP newp = new SimplePP(mypos,currentDestination,4);
+                                SimplePP newp = new SimplePP(mypos, currentDestination, 4);
                                 path = newp.getPath();
                                 //System.out.println("my position is :" + mypos);
                                 //System.out.println("going to :" + currentDestination);
                                 System.out.println("  ");
                                 System.out.println("path is: ");
                                 System.out.println("peek is " + path.peek());
-                                for (int i=path.size()-1; i>=0 ; i--){
+                                for (int i = path.size() - 1; i >= 0; i--) {
                                     System.out.println(path.get(i));
                                 }
                                 System.out.println("end of path");
@@ -181,9 +180,8 @@ public class FollowApp extends LogicThread {
 
                                 if (updatePath) {
 
-                                }
-                                else{
-                                    RobotMessage pathmsg = new RobotMessage("ALL", name, PATH_MSG, gvh.gps.getMyPosition().toString()+"###mypos");
+                                } else {
+                                    RobotMessage pathmsg = new RobotMessage("ALL", name, PATH_MSG, gvh.gps.getMyPosition().toString() + "###mypos");
                                     gvh.comms.addOutgoingMessage(pathmsg);
                                     updatePath = false;
 
@@ -202,7 +200,7 @@ public class FollowApp extends LogicThread {
                         //System.out.println(obs);
                         //System.out.println("posL\n" + pos);
                         //gvh.plat.reachAvoid.doReachAvoid(gvh.gps.getMyPosition(), currentDestination,obs);
-                        System.out.println("calling goto for point: "+ currentDestination);
+                        System.out.println("calling goto for point: " + currentDestination);
 
                         gvh.plat.moat.goTo(currentDestination);
                         stage = Stage.GO;
@@ -215,13 +213,13 @@ public class FollowApp extends LogicThread {
 
                     break;
                 case GO:
-                    System.out.println("IN GO STAGE " +name);
+                    System.out.println("IN GO STAGE " + name);
                     if (!gvh.plat.moat.inMotion) {
                         if (!gvh.plat.moat.done && currentDestination != null) {
                             stage = Stage.GO;
                         } else {
                             path.pop();
-                            if(path.empty()) {
+                            if (path.empty()) {
                                 System.out.println("GOING BACK TO PICK " + name);
                                 stage = Stage.PICK;
                             } else {
@@ -281,7 +279,7 @@ public class FollowApp extends LogicThread {
             String name = Integer.toString(i) + "-A";
             ItemPosition p = new ItemPosition(name, x, y, z);
             destinations.put(p.getName(), p);
-            taskLocations.put(p.getName(),new Task(p,i));
+            taskLocations.put(p.getName(), new Task(p, i));
 
         }
 /*
@@ -310,11 +308,6 @@ public class FollowApp extends LogicThread {
         }*/
 
 
-
-
-
-
-
     }
 
     private void updatedests(String filename, int msgtype, String robotname, int lineno) {
@@ -335,20 +328,66 @@ public class FollowApp extends LogicThread {
         return map.get(key);
     }
 
-    private <X,T> T getTask(Map <X,T> map, int index) {
-        String key = Integer.toString(index)+ "-A";
+    private <X, T> T getTask(Map<X, T> map, int index) {
+        String key = Integer.toString(index) + "-A";
         return map.get(key);
     }
 
 
-    private <X,T> void getUnassignedTask(Map <X,T> map) {
+    private <X, T> void getUnassignedTask(Map<X, T> map) {
         Iterator it = map.values().iterator();
         while (it.hasNext()) {
-            System.out.println("here "+ it.next().toString());
+            System.out.println("here " + it.next().toString());
 
 
         }
 
+
     }
 
+    private boolean isClose(Stack<ItemPosition> pathstack, Stack<ItemPosition> obstack, double mindist) {
+        int i = pathstack.size();
+        int k = obstack.size();
+
+        for (int j = 1; j < i; j++) {
+            ItemPosition start = pathstack.get(j - 1);
+            ItemPosition next = pathstack.get(j);
+            if (k == 1) {
+                ItemPosition robotPos = obstack.pop();
+                double x1 = (double) robotPos.x;
+                double y1 = (double) robotPos.y;
+                double z1 = (double) robotPos.z;
+                double x2 = (double) start.x;
+                double y2 = (double) start.y;
+                double z2 = (double) start.z;
+                double x3 = (double) next.x;
+                double y3 = (double) next.y;
+                double z3 = (double) next.z;
+
+                double b = Math.sqrt(Math.pow((x2 - x3), 2)
+                        + Math.pow((y2 - y3), 2)
+                        + Math.pow((z2 - z3), 2));
+
+                double S = Math.sqrt(Math.pow((y2 - y1) * (z3 - z1) - (z2 - z1) * (y3 - y1), 2) +
+                        Math.pow((z2 - z1) * (x3 - x1) - (x2 - x1) * (z3 - z1), 2) +
+                        Math.pow((x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1), 2)) / 2;
+
+                double distance = 2 * S / b;
+                if (distance <= mindist) return true;
+                else continue;
+            } else {
+                for (int p = 1; p < k; p++) {
+                    ItemPosition obstart = obstack.get(p - 1);
+                    ItemPosition obnext = obstack.get(p);
+
+                    double distance = 0;
+
+                    if (distance <= mindist) return true;
+                    else continue;
+                }
+            }
+        }g
+        return false;
+
+    }
 }
