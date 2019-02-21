fad2ca5eaa22499ec9d730a340c4b13205900540
Peter Du <peterdu2@illinois.edu>
2019-02-21 15:10:09 -0600

    pushing bot1

--- src/main/java/testmain/BotInfoSelector.java
+++ src/main/java/testmain/BotInfoSelector.java
@@ -88,9 +88,9 @@ public class BotInfoSelector {
 
         if(color.equals("blue")) {
             name = "bot2";
-            vrpn = "vrpn_client_node/cyphyhousecopter1/pose";
+            vrpn = "vrpn_client_node/f1car/pose";
             //ip = "192.168.1.38";
-            ip = "192.168.1.14";
+            ip = "192.168.1.18";
             if(type == Common.IROBOT) {
                 bluetooth = "5C:F3:70:76:CE:B4";
                 this.type = new Model_iRobot(name, 0,0);
